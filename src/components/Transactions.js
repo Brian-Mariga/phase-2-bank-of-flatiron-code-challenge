408dfeb459c7ad4063df29530df3ae663393a0fc
Brian <brianmariga0@gmail.com>
2024-02-04 19:42:55 +0300

    Progress

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from "react";
 
 function Transactions() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/transactions")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
+  return (
+    <>
+      <table>
+        <thead>
+          <th style={{ border: "1px solid black" }}>ID</th>
+          <th style={{ border: "1px solid black" }}>Date</th>
+          <th style={{ border: "1px solid black" }}>Description</th>
+          <th style={{ border: "1px solid black" }}>Category</th>
+          <th style={{ border: "1px solid black" }}>Amount</th>
+        </thead>
+        <tbody>
+          {data.map((trans) => (
+            <tr key={trans.id}>
+              <td style={{ border: "1px solid black" }}>{trans.id}</td>
+              <td style={{ border: "1px solid black" }}>{trans.date}</td>
+              <td style={{ border: "1px solid black" }}>{trans.description}</td>
+              <td style={{ border: "1px solid black" }}>{trans.category}</td>
+              <td style={{ border: "1px solid black" }}>{trans.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Transactions;
