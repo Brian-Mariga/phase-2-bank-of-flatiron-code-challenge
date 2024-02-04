646af63fbf1326056452b7ce412362760a6226ff
Brian <brianmariga0@gmail.com>
2024-02-04 17:43:31 +0300

    Progress

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransaction() {
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  function handleClear() {
+    setDate("");
+    setDescription("");
+    setAmount("");
+    setCategory("");
+  }
+
   return (
-    <form id="form">
+    <form id="form" onSubmit={handleSubmit}>
       <label htmlFor="date">Date:</label>
       <input
         type="text"
@@ -13,13 +28,34 @@ function AddTransaction() {
         title="Please enter a date in the format YYYY-MM-DD"
       />
       <label htmlFor="description">Description:</label>
-      <input type="text" id="description" placeholder="Description" />
+      <input
+        type="text"
+        id="description"
+        placeholder="Description"
+        required
+        title="Please enter the description of the transaction"
+      />
       <label htmlFor="category">Category:</label>
-      <input type="text" id="category" placeholder="Category" />
+      <input
+        type="text"
+        id="category"
+        placeholder="Category"
+        required
+        title="Please enter the category of the transaction"
+      />
       <label htmlFor="amount">Amount:</label>
-      <input type="number" id="amount" placeholder="Amount" />
+      <input
+        type="number"
+        id="amount"
+        placeholder="Amount"
+        required
+        title="Please enter the amount"
+      />
 
       <button type="submit">Add Transaction</button>
+      <button type="submit" onClick={handleClear}>
+        Clear
+      </button>
     </form>
   );
 }
